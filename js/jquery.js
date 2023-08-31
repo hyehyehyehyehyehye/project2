f85284e83e375124bd6410df5f1d9de764b85c91
hyehyehyehyehyehye <142138962+hyehyehyehyehyehye@users.noreply.github.com>
2023-08-31 17:57:35 +0900

    js 추가

--- js/jquery.js
+++ js/jquery.js
@@ -20,3 +20,46 @@ $(".btn_close").on("click", function (e) {
   $(".popup").hide();
   $(".overlay").removeClass("open");
 });
+
+//
+// setInterval(topSlide, 3000);
+// function topSlide() {
+//   $(".dep2_banenr_wrap ").animate(
+//     { "margin-left": "-320px" },
+//     1200,
+//     function () {
+//       $(".dep2_banenr_wrap ").css({ "margin-left": "0" });
+//       $(".dep2_banenr_wrap>img:first-child").insertAfter(
+//         ".dep2_banenr_wrap>img:last-child"
+//       );
+//     }
+//   );
+// }
+
+setInterval(fnSlide, 4000);
+function fnSlide() {
+  $(".main_prd_wrap .inner ul").animate(
+    { "margin-left": "-430px" },
+    1200,
+    function () {
+      $(".main_prd_wrap .inner ul").css({ "margin-left": "0" });
+      $(".main_prd_wrap .inner ul li:first-child").insertAfter(
+        ".main_prd_wrap .inner ul li:last-child"
+      );
+    }
+  );
+}
+
+setInterval(underSlide, 3000);
+function underSlide() {
+  $(".review_wrap .inner ul").animate(
+    { "margin-left": "-250px" },
+    1200,
+    function () {
+      $(".review_wrap .inner ul").css({ "margin-left": "0" });
+      $(".review_wrap .inner ul li:first-child").insertAfter(
+        ".review_wrap .inner ul li:last-child"
+      );
+    }
+  );
+}
